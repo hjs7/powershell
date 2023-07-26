87d08060bfe92143d21888177721510ba413a48a
hjs7 <132304550+hjs7@users.noreply.github.com>
2023-07-27 06:48:47 +1000

    Add files via upload

---
+++ calendar_find.ps1
@@ -0,0 +1,23 @@
+﻿## Usage: .\calendar_find.ps1 [keyword]
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$searchstring
+)
+
+$allmailboxes = (get-EXOmailbox)
+
+foreach ($mailbox in $allmailboxes){
+
+    $mailboxaddress = $mailbox.UserPrincipalName
+    $listcalendars = (Get-EXOMailboxFolderStatistics -Identity $mailboxaddress -Folderscope calendar).Name
+    
+    If ($listcalendars -contains $searchstring)
+    {
+        Write-Host "Calendar belongs to account: " $mailboxaddress
+   
+    }
+    Else {
+        Write-Host "** Calendar not found in account " $mailboxaddress "**" -f Red
+        }
+}
