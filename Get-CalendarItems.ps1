84a15c743e9d002c67b94d039166367fc62d236f
Simon <simonlavigne@hotmail.com>
2023-07-27 10:19:05 +1000

    -contains operator to -like

--- Get-CalendarItems.ps1
+++ Get-CalendarItems.ps1
@@ -1,23 +1,40 @@
-﻿## Usage: .\Get-CalendarItems.ps1 [keyword]
+﻿<#
+    .SYNOPSIS
+        Searches for a calendar with a specific name in all Exchange Online mailboxes.
+
+    .DESCRIPTION
+        The Get-CalendarItems script searches for a calendar with a specific name in all Exchange Online mailboxes. If a calendar with the specified name is found, the script outputs the email address of the mailbox that contains the calendar.
+
+    .PARAMETER SearchString
+        The name of the calendar to search for.
+
+    .EXAMPLE
+        Get-CalendarItems -SearchString "Team Meetings"
+
+        This example searches for a calendar named "Team Meetings" in all Exchange Online mailboxes.
+
+    .NOTES
+        Author: 
+#>
 
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$SearchString
+    [Parameter(Mandatory = $true)]
+    [string] $SearchString,
+
+    [Parameter()]
+    [bool] $ShowWarnings = $true
 )
 
-$allMailBoxes = (get-EXOmailbox)
+$allMailBoxes = Get-EXOMailbox -ResultSize Unlimited -ErrorAction Stop
 
-foreach ($mailBox in $allMailBoxes){
-
+foreach ($mailBox in $allMailBoxes) {
     $mailBoxAddress = $mailBox.UserPrincipalName
-    $listCalendars = (Get-EXOMailboxFolderStatistics -Identity $mailBoxAddress -Folderscope calendar).Name
+    $listCalendars = (Get-EXOMailboxFolderStatistics -Identity $mailBoxAddress -Folderscope calendar -ErrorAction Stop).Name
 
-    # Supposed to search the listcalendars variable for the SearchString, however it's never found 
-    if ($listCalendars -contains $SearchString)
-    {
-        Write-Host "Calendar belongs to account: " $mailBoxAddress
+    if ($listCalendars -like $SearchString) {
+        Write-Information "Calendar '$SearchString' belongs to account: $mailBoxAddress" -InformationAction Continue
     }
-    else {
-        Write-Host "** Calendar not found in account " $mailBoxAddress "**" -f Red
-        }
+    elseif ($ShowWarnings) {
+        Write-Warning "Calendar '$SearchString' not found in account $mailBoxAddress"
+    }
 }
